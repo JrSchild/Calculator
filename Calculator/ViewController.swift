64e468258ae4b03d04fb74159fed1faa213e23da
JrSchild <joriami@gmail.com>
2015-04-02 14:44:45 +0200

    init commit, 21 minutes into Lecture 2.

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Joram Ruitenschild on 02-04-15.
+//  Copyright (c) 2015 Joram Ruitenschild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
